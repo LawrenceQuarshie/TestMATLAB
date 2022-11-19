97db1e29b74975f4a9fae87132f2bf1a25fbae4d
Kojo <lquarshie29@gmail.com>
2022-11-18 23:04:55 -0500

    Added another MATLAB which calculate the trig of values in degrees and radians

---
+++ a02.m
@@ -0,0 +1,9 @@
+%% Using Angles in Degrees and Radians on MATLAB
+% First on Degrees - use tand, cosd, sind
+a = sind(360)
+aa = cosd(270)
+
+% Second on Radians - use tanpi, cospi, sinpi
+% pi is 180
+b = sinpi(2)
+bb = cospi(3/2)
