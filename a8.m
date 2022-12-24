a0ea70504591d515e3b71ea3305205d8f7ed1497
Kojo <lquarshie29@gmail.com>
2022-12-24 11:55:16 -0500

    Added a practise exercise with matrix addition

---
+++ a8.m
@@ -0,0 +1,9 @@
+a = [5 3 1];
+b = [1 3 5];
+
+a_plus_b = a + b
+
+c = 5:-2:1; % row matrix from 5 to 1 with decreasing interval of 2
+d = 1:2:5; % row matrix from 1 to 5 with increasing interval of 2
+
+c_plus_d = c + d
