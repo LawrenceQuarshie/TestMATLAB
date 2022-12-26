891ed02fc9343ea37bab4829893e656efd6c96db
Kojo <lquarshie29@gmail.com>
2022-12-25 22:36:53 -0500

    Added a .m file which adds, subtract, multiplies, divides and squares the individual element in two different arrays

---
+++ a8v2.m
@@ -0,0 +1,23 @@
+clc, clear % clears all stored data in command window and workspace
+
+x = [1, 3, 5]
+y = [5, 3, 1]
+
+x_minus_y = x - y
+
+x_times_y = x .* y
+
+x_divided_by_y = x./y
+
+x_squared = x .^2
+
+u = 1:2:5;
+w = 5:-2:1;
+
+u_minus_w = u - w
+
+u_times_w = u .* w
+
+u_divided_by_w = u./w
+
+w_squared = w .^2
