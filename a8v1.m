d01ba683c833f72a1d2b06ff4f6ed2fbe37de572
Kojo <lquarshie29@gmail.com>
2022-12-27 21:09:34 -0500

    Added .m file which generates a matrix which prints rows and columns

---
+++ a8v1.m
@@ -0,0 +1,7 @@
+clc, clear
+
+norfolk_temp = [-1 -1 -1;-1 -2 -2;-2 -3 -3] % a matrix of temperatures in Norfolk on 27/12/2022
+
+first_row = norfolk_temp(1,:) % prints the first row
+
+second_col = norfolk_temp(:,2) % prints the second column
