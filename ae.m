9064bb40a84774fe2bbcfbbdd072c6b2d2bed9a9
Kojo <lquarshie29@gmail.com>
2023-01-07 00:17:40 -0500

    Added a comment the file

--- ae.m
+++ ae.m
@@ -4,6 +4,8 @@ close all
 
 %% Declaration of variables 
 n = 0:0.01:8;
+
+%% Visualisation
 semilogy(n,n, 'b', n,n.^2, 'r', n,n.^3, 'g', n,2.^n, 'c', n,exp(n),'k')
 title('Big-O characteristics: Logarithmic Plot')
 ylabel('Estimate of Running Time')
