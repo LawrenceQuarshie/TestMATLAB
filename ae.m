eec23c7994dce295cbf0536a6dba48b643e83ace
Kojo <lquarshie29@gmail.com>
2023-01-07 00:15:54 -0500

    Created a file that uses the semilogy for plotting graph.

---
+++ ae.m
@@ -0,0 +1,12 @@
+clc
+clear
+close all
+
+%% Declaration of variables 
+n = 0:0.01:8;
+semilogy(n,n, 'b', n,n.^2, 'r', n,n.^3, 'g', n,2.^n, 'c', n,exp(n),'k')
+title('Big-O characteristics: Logarithmic Plot')
+ylabel('Estimate of Running Time')
+xlabel('n (number of elements)')
+legend('O(n)', 'O(n^2)', 'O(n^3)', 'O(2^n)', 'O(e^n)', 'Location', 'southeast')
+grid on
