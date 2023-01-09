cdb7d19d35899bff68ada40db0a71f6893cbdf3e
Kojo <lquarshie29@gmail.com>
2023-01-09 16:40:41 -0500

    Added a .m file which calculates the area of a circle and prints that to the screen.

---
+++ ah.m
@@ -0,0 +1,13 @@
+%% Calculating Area of a Circle
+clc
+clear
+close all
+
+%% Declaration of variables
+
+radius = 2.5 % Radius of circle
+area = pi * radius^2; % Area of the circle
+string = ['The area of the circle is ', num2str(area)];
+
+%% Visualization of results
+display(string) % Prints the output to the screen
