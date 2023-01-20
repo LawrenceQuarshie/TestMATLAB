99e102b2a9963a3c27664aaaa953c4b0982f6527
Kojo <lquarshie29@gmail.com>
2023-01-20 00:17:21 -0500

    Added a .m file which demonstrates the solution to the problem ((1/(5^2)) - ((3/2) * pi) - 1) ^ -3

---
+++ aiv1.m
@@ -0,0 +1,10 @@
+clc
+clear
+close all
+
+%% Define variables and parameters
+a = ((1/(5^2)) - ((3/2) * pi) - 1) ^ -3;
+
+%% Visualisation of results
+output_text = ['The solution for ((1/(5^2)) - ((3/2) * pi) - 1) ^ -3 is ', num2str(a)];
+disp(output_text)
