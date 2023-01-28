58e987e58c578cd9f0bf8eb0527600d049c21abe
Kojo <lquarshie29@gmail.com>
2023-01-28 04:15:48 -0500

    Added a .m file which demonstrates the operation 1 + (1/2) + (1/2^2) + (1/3^3) + (1/2^4)

---
+++ aiv3.m
@@ -0,0 +1,10 @@
+clc
+clear
+close all
+
+%% Define variables and parameters
+y = 1 + (1/2) + (1/2^2) + (1/3^3) + (1/2^4);
+
+%% Visualisation of results
+outputtext = ['The solution for (1 + (1/2) + (1/2^2) + (1/3^3) + (1/2^4)) is ', num2str(y)];
+disp(outputtext)
