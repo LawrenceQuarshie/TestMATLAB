600c3af08f187f040a56212a403336fba20f1759
Kojo <lquarshie29@gmail.com>
2023-01-28 03:22:38 -0500

    Added a .m file which performs the operation on 2*pi - pi^0.5 and prints the solution to the screen.

---
+++ aiv2.m
@@ -0,0 +1,10 @@
+clc
+clear
+close all
+
+%% Define variables and parameters
+x = 2*pi - pi^0.5;
+
+%% Visualisation of results
+output_text = ['The solution for (2*pi - pi^0.5) is ', num2str(x)];
+disp(output_text)
