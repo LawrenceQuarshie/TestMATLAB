122cde7d65263cfab4be4eb5c0ffb989488d34a9
Kojo <lquarshie29@gmail.com>
2023-02-24 19:04:23 -0500

    Added a .m file which calculates the perimeter and the circumference of the ellipse.

---
+++ aiv13.m
@@ -0,0 +1,18 @@
+clc
+clear
+close all
+
+%% Define variables and parameters of the ellipse
+a = 5; % value of a
+b = 10; % value of b
+
+%% Calculation of intermediate perimeter of an ellipse
+h = ((a - b)^2)/((a + b)^2);
+
+%% Calculation of approximate circumference of an ellipse
+C = pi*(a + b)*(1 + (3*h)/(10 + sqrt(4 - 3)));
+
+%% Visualisation of results
+output1 = ['The intermediate perimeter and approximate circumference'...
+    ' of an ellipse is ', num2str(h), ' and ', num2str(C), ' respectively.'];
+disp(output1)
