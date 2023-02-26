02214b225b86f8c510a65073016122d80d893d8d
Kojo <lquarshie29@gmail.com>
2023-02-26 07:46:55 -0500

    Added a .m file which calculates the area of a circle, volume of a sphere and surface area of a sphere given different values of radius r

---
+++ circle_and_sphere.m
@@ -0,0 +1,12 @@
+clc
+clear
+close all
+
+%% Calculations
+r = 20; % use different values of radius (r) = 1, 5, 10, 20
+
+area_circle = pi*r^2
+
+volume_sphere = 4/3*pi*r^3
+
+area_sphere = 4*pi*r^2
