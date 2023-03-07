212c5e42563b8279ae2c18dc449c563d51853fe2
Kojo <lquarshie29@gmail.com>
2023-03-07 09:30:09 -0500

    Edited the line for calculating the circumference of an ellipse

--- aiv13.m
+++ aiv13.m
@@ -10,7 +10,7 @@ b = 10; % value of b
 h = ((a - b)^2)/((a + b)^2);
 
 %% Calculation of approximate circumference of an ellipse
-C = pi*(a + b)*(1 + (3*h)/(10 + sqrt(4 - 3)));
+C = pi*(a + b)*(1 + (3*h)/(10 + sqrt(4 - 3*h)));
 
 %% Visualisation of results
 output1 = ['The intermediate perimeter and approximate circumference'...
