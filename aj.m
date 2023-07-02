6373fdfb717a79aeb728598b3fe62379de65bde8
Kojo <lquarshie29@gmail.com>
2023-07-02 01:29:31 -0400

    Adding conservative numbers in a sequence

--- aj.m
+++ aj.m
@@ -1,6 +1,6 @@
 %% Summation of Integers from 0 to 5
 % Declaration of variables
-N = 5; % total number of iterations
+N = 1000; % total number of iterations
 
 b = 0; % initial value of b
 for k = 1:N
