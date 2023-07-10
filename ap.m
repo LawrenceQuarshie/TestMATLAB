7a4d119034f4b9371e9ca3cedf227e9ef776de35
Kojo <lquarshie29@gmail.com>
2023-07-09 23:00:19 -0400

    Added a .m file which demonstrates the use disp as a way to get an output in MATLAB

---
+++ ap.m
@@ -0,0 +1,11 @@
+clc
+clear
+close all
+
+radius = input('Enter circle radius:\n');
+area = pi * radius^2;
+str = ['The area is ', num2str(area)];
+disp(str)
+
+value = int2str(pi);
+disp(['The value is ' value '!']);
