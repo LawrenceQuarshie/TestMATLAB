997ca0cc558b1a31a9bc4d46ec48f5c3b60c6ae2
Kojo <lquarshie29@gmail.com>
2023-07-10 23:31:47 -0400

    Added a .m file that demonstrates the use of fprintf in MATLAB.

---
+++ aq.m
@@ -0,0 +1,10 @@
+%% MATLAB Programming for Engineers; Quiz 2.2; Q3
+clc
+clear
+close all
+
+value = 123.4567e2;
+fprintf('value = %e\n',value);
+fprintf('value = %f\n',value);
+fprintf('value = %g\n',value);
+fprintf('value = %12.4f\n',value);
