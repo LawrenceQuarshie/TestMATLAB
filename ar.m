2ac185e08706a13e51b647f00a8a7f4c5a375a5d
Kojo <lquarshie29@gmail.com>
2023-07-14 07:17:05 -0400

    Added a .m file which performed mathematical operations (addition, subtraction, multiplication, divison, etc) on matrices

---
+++ ar.m
@@ -0,0 +1,13 @@
+%% MATLAB Programming for Engineers; Example 2.1
+clc
+clear
+close all
+
+a = [1 0; 2 1];
+b = [-1 2; 0 1];
+c = [3; 2];
+d = 5;
+
+% Example 2.1a
+a + b % ans = [0 2; 2 2]
+a .* b % ans = [-1 0; 0 1]
