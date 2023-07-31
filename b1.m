67fd643d78e15e19a78f67d7130d31074ad08292
Kojo <lquarshie29@gmail.com>
2023-07-31 01:26:09 -0400

    Added a .m file that demonstrates the vector addition and subtraction with MATLAB

---
+++ b1.m
@@ -0,0 +1,15 @@
+%% Vector Addition and Subtraction
+clc
+clear
+close all
+
+% v1 is a vector v1 = 3i + 4j + 5k
+% v2 is a vector v2 = -4i + 3j + 2k
+v1 = [3 4 5];
+v2 = [-4 3 2];
+
+% Vector addition
+va = v1 + v2
+
+% Vector subtraction
+vs = v1 - v2
