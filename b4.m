b28903d72f5ab5d3276507c5e9c97561f7e1b7a1
Kojo <lquarshie29@gmail.com>
2023-08-04 01:02:49 -0400

    Added a .m file demonstrating the scalar dot product of vectors in MATLAB.

---
+++ b4.m
@@ -0,0 +1,10 @@
+%% 2.13.2 Scalar (Dot) Product of Vectors
+clc
+clear
+close all
+
+C = [2 3 1];
+D = [3 -2 2];
+
+R = dot(C, D);
+disp(['The scalar (dot) product of vector C and D is ',num2str(R),' meters squared'])
