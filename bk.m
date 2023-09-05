4fd05182d33671c2af6f8ce309886e2e9a86b917
Kojo <lquarshie29@gmail.com>
2023-09-04 23:09:08 -0400

    Added a .m file that demonstrates operations, sequence of operation, and approximations.

---
+++ bk.m
@@ -0,0 +1,32 @@
+%% 2.17 Exercises - 2.7
+clear
+clc
+
+% Evaluate each of the following expressions.
+
+% 2.7a
+12 / 5 + 4
+
+% 2.7b
+(12 / 5) + 4
+
+% 2.7c
+12 / (5 + 4)
+
+% 2.7d
+3 ^ 2 ^ 3
+
+% 2.7e
+3 ^ (2 ^ 3)
+
+% 2.7f
+(3 ^ 2) ^ 3
+
+% 2.7g
+round(-12 / 5) + 4
+
+% 2.7h
+ceil(-12 / 5) + 4
+
+% 2.7i
+floor(-12 / 5) + 4
