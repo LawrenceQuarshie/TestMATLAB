d302a60dd691853ca8004a328490d0da8d8c9641
Kojo <lquarshie29@gmail.com>
2023-09-30 08:05:58 -0400

    Updated and made corrections to the script for polartorectangular and rectangulartopolar conversions

--- bs.m
+++ bs.m
@@ -2,20 +2,20 @@
 
 
 % 2.13 a. Calculates the vector in polar coordinates, with the angle u
-% expressed in degrees.
-rect_coord_inputs = input("Enter an array for rectangular coordinates: ");
+% expressed in radians.
+rect_coord_inputs = input("Enter an array for rectangular coordinates: "); % this must be an 1 x 2 matrix
 rect_coordinates = rect_coord_inputs;
 r = sqrt(rect_coordinates(1)^2 + rect_coordinates(2)^2);
 
-theta = atand(rect_coordinates(2)/rect_coordinates(1));
+theta = atan2(rect_coordinates(2),rect_coordinates(1)) * 180/pi;
 
-disp(['The magnitude of the vector is ', num2str(r), ' and the angle (theta) is ', num2str(theta)])
+disp(['The magnitude of the vector is ', num2str(r), ' and the angle (theta) is ', num2str(theta), ' degrees.'])
 
 % 2.13 b. Calculates the vector in rectangular coordinates
-polar_coordinates = input("Enter the magnetic and angle (in degrees): ");
+polar_coordinates = input("Enter the magnitude and angle (in degrees): "); % this must be a 1 x 2 matrix
 r2 = polar_coordinates(1);
 theta2 = polar_coordinates(2);
-x = r2 * cosd(theta2);
-y = r2 * sind(theta2);
+x = r2 * cos(theta2 * pi/180);
+y = r2 * sin(theta2 * pi/180);
 
 disp(['The values of the rectangular coordinates are ', num2str(x), ' and ', num2str(y)])
