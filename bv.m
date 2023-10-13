f590b076bdcff3c8d06a9b926b702b110f52446f
Kojo <lquarshie29@gmail.com>
2023-10-13 07:07:15 -0400

    Added a portion of code for the conversion of rectangular coordinates to spherical coordinates

---
+++ bv.m
@@ -0,0 +1,12 @@
+%% 2.17 Exercises - 2.16
+clear
+clc
+
+% 2.16. This program converts a three (3) dimensional vector in rectangular
+% coordinates into spherical coordinates and vice versa.
+
+x = input('Enter a value for x: ');
+y = input('Enter a value for y: ');
+z = input('Enter a value for z: ');
+
+r = sqrt((x^2) + (y^2) + (z^2));
